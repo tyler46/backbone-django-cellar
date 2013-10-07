79445d9f6ac4a7777b4c0f71f16decf20e1d10b8
spyros <spyros@movinapp.com>
2013-10-07 22:59:30 +0300

    Add factory_boy to tests.

--- cellar/settings/base.py
+++ cellar/settings/base.py
@@ -113,6 +113,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'south',
     'tastypie',
+    'product_details',
     'cellar.wines',
     'cellar.api',
 )
--- cellar/wines/tests.py
+++ cellar/wines/tests.py
@@ -1,16 +1,35 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+import os
+import random
+import string
 
-Replace this with more appropriate tests for your application.
-"""
+from django.utils.timezone import utc
 
-from django.test import TestCase
+import factory
+from factory import fuzzy
+from product_details import product_details
+
+from .models import Wine
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+COUNTRIES = product_details.get_regions('en').values()
+YEAR_START = datetime.datetime(2000, 1, 1, tzinfo=utc)
+YEAR_END = datetime.datetime(2013, 1, 1, tzinfo=utc)
+
+# Assume there is a wine_bottle.jpn next to our tests.py
+TEST_IMAGE = os.path.join(os.path.dirname(__file__), 'wine_bottle.jpg')
+
+
+def random_string(length=20):
+    return u''.join(random.choice(string.ascii_letters) for x in range(length))
+
+
+class WineFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = Wine
+
+    name = factory.Sequence(lambda n: 'Wine %s' % n)
+    grapes = factory.Sequence(lambda n: 'Grapes %s' % n)
+    country = fuzzy.FuzzyChoice(COUNTRIES)
+    region = 'RegionName'
+    year = fuzzy.FuzzyDateTime(YEAR_START, YEAR_END)
+    notes = factory.Sequence(lambda n: random_string())
+    picture = factory.django.ImageField(from_file=TEST_IMAGE)
